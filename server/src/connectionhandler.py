2fc8bbe0930dae8496836149fb5cdd3b34e16f2e
Julien Thieffry <julien.thieffry@eamslab.com>
2018-08-03 20:06:50 +0900

    connectionhandler.py: implement main logic.

--- server/src/connectionhandler.py
+++ server/src/connectionhandler.py
@@ -35,3 +35,28 @@ class ConnectionHandler:
     def setfilename(self, filename):
         """Set the filename."""
         self.filename = filename
+
+    def initialize(self):
+        """Initialize the connection. Return a socket."""
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((self.ip, self.port))
+        return server_socket
+
+    def start_server(self, server_socket):
+        """Listen and treat incoming connections."""
+        while True:
+            server_socket.listen(5)
+            print("Waiting for incoming connections...")
+
+            client, addr = server_socket.accept()
+            print("Incoming connection from " + str(addr))
+
+            with open(self.filename, 'wb') as img:
+                while True:
+                    data = client.recv(1024)
+                    if not data:
+                        break
+                    img.write(data)
+
+            print("Transfer Completed.")
+            print("Closing " + str(addr))
