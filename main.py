483cf78b46da4322200bc4d081f878de0bf6eda9
Julien Thieffry <julien.thieffry@eamslab.com>
2018-08-06 12:09:07 +0900

    main.py: modifying default IP and fixing the main server loop.

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 from server.connectionhandler import ConnectionHandler
 from engine.facedetect import FaceDetect
 
-connectd = ConnectionHandler()
-socket = connectd.initialize()
+connectd = ConnectionHandler(ip="172.31.33.238")
 
 while True:
+    socket = connectd.initialize()
     connectd.start_server(socket)
+    socket.close()
+    print("Starting detection...")
     faced = FaceDetect('default.jpg')
-    faced.convertgrey()
     faced.detect()
