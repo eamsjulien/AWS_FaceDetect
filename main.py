1704cff0ac09d09fb0c50eaec4553a58f58b1ec2
Julien Thieffry <julien.thieffry@eamslab.com>
2018-08-07 12:07:09 +0900

    main.py: adding rectangle detect and file construc in the main loop.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 """Main handler for the AWS server component."""
 
+import os
+import json
+
 from server.connectionhandler import ConnectionHandler
 from engine.facedetect import FaceDetect
 
 connectd = ConnectionHandler(ip="172.31.33.238")
+img_loc = os.path.join(os.environ['AWS_FLASK_FOLDER'], 'aws/static', 'client_img/default.jpg')
+json_loc = os.path.join(os.environ['AWS_FLASK_FOLDER'], 'aws/static', 'json/current.json')
 
 while True:
     socket = connectd.initialize()
@@ -11,4 +16,10 @@ while True:
     socket.close()
     print("Starting detection...")
     faced = FaceDetect('default.jpg')
-    faced.detect()
+    faces = faced.detect()
+    faced.image = faced.drawrectangle(faces)
+    faced.convertrgb
+    faced.saveimage(img_loc)
+    faces_dict = {'count':len(faces)}
+    with open(json_loc, 'w') as js:
+        json.dump(faces_dict, js)
