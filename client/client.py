5a5018460a183b12dd4545259bbefd88ee1a5a2f
Julien Thieffry <julien.thieffry@eamslab.com>
2018-08-09 16:54:50 +0900

    client: reworking client interfaces and main loop.

--- client/client.py
+++ client/client.py
@@ -1,7 +1,46 @@
 """Client class for remote hosts ready to send images to AWS."""
 
 import socket
+import os
+import time
 
+
+def init_facedetect_environ_folder(capture_loc=None):
+    """Return necessary capture_loc based on environ."""
+    if capture_loc is None:
+        capture_loc = os.path.join(os.environ['AWS_FACEDETECT_FOLDER'], 'client/capture/')
+    return capture_loc
+
+def init_client_socket(address, port=None):
+    """Initialize client socket. Address is mandatory argument, default port is 5000."""
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((address, port))
+    return client_socket
+
+def send_total_frame_nbr(client_socket, frame_nbr):
+    """Send frame number to server."""
+    client_socket.send(str(frame_nbr).encode('ascii'))
+
+def send_frame_size(client_socket, frame_loc):
+    """Send frame size to client."""
+    filesize = os.path.getsize(frame_loc)
+    client_socket.send(str(filesize).encode('ascii'))
+
+def send_frame(client_socket, frame_loc, sleep=1):
+    """Send frame to client with a default sleep of 1s."""
+    time.sleep(sleep)
+    with open(frame_loc, 'rb') as f:
+        buf = f.readline(1024)
+        while buf:
+            client_socket.send(buf)
+            buf = f.readline(1024)
+        f.close()
+
+def waiting_for_ack(client_socket, frame):
+    """Wait for a particular frame to be acked by server."""
+    msg = client_socket.recv(1024).decode('UTF-8')
+    while (msg != 'OK FRAME ' + str(frame)):
+        msg = client_socket.recv(1024).decode('UTF-8')
 
 class Client:
     """Takes optional arguments. Handle incoming connections."""
--- main_client.py
+++ main_client.py
@@ -1,20 +1,44 @@
 """Main handler for the client component."""
 
-import os
-
-from client.client import Client
+import client.client as cl
 from client.camera import Camera
 
-FRAME_NBR = 200
+FRAME_NBR = 20
+SERVER_ADDR = '13.115.118.145'
 
-capture_loc = os.path.join(os.environ(['AWS_FACEDETECT_FOLDER']), 'client/capture/')
+print(" -------------------------")
+print("| AWS FACEDETECT _ SERVER |")
+print(" -------------------------")
 
+print("\n Initializing ENV variables...", end='')
+capture_loc = cl.init_facedetect_environ_folder()
+print("Done!")
+
+print("\n Initializing server socket...", end='')
+client_socket = cl.init_client_socket(SERVER_ADDR)
+print("Done!")
+
+print("\n **** CAPTURING FRAMES ****")
 cam = Camera(frames=FRAME_NBR, path=capture_loc)
 cam.capture()
+print(" " + str(FRAME_NBR) + " frames captured!")
 
-for nbr in range(0, FRAME_NBR):
-    filen = "frame" + nbr + ".jpg"
-    client = Client(filen, ip="18.222.248.133")
-    socketd = client.initialize()
-    client.send_data(socketd)
-    socketd.close()
+print("\n **** SENDING FRAMES NBR ****")
+cl.send_total_frame_nbr(client_socket, FRAME_NBR)
+print(" Frames number sent to server!")
+
+print("\n **** SENDING FRAMES ****")
+for frame in range(FRAME_NBR):
+    frame_loc = capture_loc + "frame" + str(frame) + ".jpg"
+    cl.send_frame_size(client_socket, frame_loc)
+    print("Sending frame %s..." % str(frame), end='')
+    cl.send_frame(client_socket, frame_loc, sleep=0.1)
+    print("Done.")
+    print("Waiting for frame" + str(frame) + " reception...", end='')
+    cl.waiting_for_ack(client_socket, frame)
+    print("ACK")
+
+print("\nFrames sent!")
+print("\n --------------------------")
+print("| AWS FACEDETECT - GOODBYE |")
+print(" --------------------------")
